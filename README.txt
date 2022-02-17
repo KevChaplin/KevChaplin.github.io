545e86d379ed5d023ee8b856400bd14286836bfb
KevChaplin <kevchaplin44@gmail.com>
2022-02-17 10:25:21 +0900

    japanese text added, readme fixed

--- README.txt
+++ README.txt
@@ -7,14 +7,14 @@ Adapted from "Prologue" by HTML5 UP (html5up.net | @ajlkn)
 
 ## Template includes:
 
-- Icons by Font Awesome (fontawesome.io)
-- jQuery (jquery.com)
-- Scrollex (github.com/ajlkn/jquery.scrollex)
-- Responsive Tools (github.com/ajlkn/responsive-tools)
+    - Icons by Font Awesome (fontawesome.io)
+    - jQuery (jquery.com)
+    - Scrollex (github.com/ajlkn/jquery.scrollex)
+    - Responsive Tools (github.com/ajlkn/responsive-tools)
 
 ### Addtional resources added by myself
 
-- Localization by i18Next (i18next.com)
-- Background Images from Unsplash (unsplash.com)
-- Icons by Devicon (devicon.dev)
-- Contact Form Backend by FormSubmit (formsubmit.io)
+    - Localization by i18Next (i18next.com)
+    - Background Images from Unsplash (unsplash.com)
+    - Icons by Devicon (devicon.dev)
+    - Contact Form Backend by FormSubmit (formsubmit.io)
--- assets/js/i18n.js
+++ assets/js/i18n.js
@@ -1,3 +1,18 @@
+function switchLanguage() {
+  let lang = i18next.language
+  console.log(lang)
+  // lang === 'en' ? lang = 'jp' : lang = 'en'
+  
+  // i18next.changeLanguage(lang, (err, t) => {
+  //   if (err) return console.log('something went wrong loading', err);
+  //   t('key'); // -> same as i18next.t
+  // })
+}
+
+// const rerender = () => {
+//   $('body').localize()
+// }
+
 $(function () {
     // use plugins and options as needed, for options, detail see
     // https://www.i18next.com
@@ -61,6 +76,59 @@ $(function () {
                   message: "Message"
                 },
                 button: "Submit"
+              }
+            }
+          },
+          jp: {
+            translation: {
+              nav: {
+                title: "チャップリン　ケビン",
+                subTitle: "フロンとエンドエンジニア",
+                link1: "トップ",
+                link2: "ポートフォリオ",
+                link3: "自己紹介",
+                link4: "お問い合わせ"
+              },
+              intro: {
+                title: "チャップリン　ケビン",
+                subTitle: "フロンとエンドエンジニア",
+                location: "日本",
+                button: "次へ"
+              },
+              portfolio: {
+                title: "ポートフォリオ",
+                subTitle: "生バージョンかGitHubでコードも見られます",
+                project1: {
+                  name: "翻訳者のホームページ"
+                },
+                project2: {
+                  name: "私のレーシングを評価する"
+                },
+                project3: {
+                  name: "日本の環境　ダッシュボード"
+                },
+                project4: {
+                  name: "JavaScript 計算機"
+                }
+              },
+              about: {
+                title: "自己紹介",
+                body: `Self-taught Front-End Engineer with excellent working knowledge of HTML, CSS, JavaScript, React, Material UI, Svelte, D3.js, Git and Github. 
+                Track record of excelling in any environment. 
+                Bsc (Hons) Mathematics graduate, JLPT N3 certified Japanese with a rich and diverse career. Including: 
+                Military Analyst (including two operational deployments); professional International Karate Instructor; and Prisoner Custody Officer.
+                Family man, married with two children. 
+                Click below for full resume.`
+              },
+              contact: {
+                title: "お問い合わせ",
+                subTitle: "ご連絡にお待ちしております。",
+                placeholder: {
+                  name: "名前",
+                  email: "メール",
+                  message: "メッセージ"
+                },
+                button: "送信"
               }
             }
           }
